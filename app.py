2eceb279ec497498f0b885fb944472b3a22706a9
James Li <jamespeilunli@gmail.com>
2024-10-21 16:23:34 -0700

    split frontend into /depression and /anxiety pages to prepare for anxiety model

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/depression")
+def depression():
+    return render_template("depression.html")
+
+
 @app.route("/about-us")
 def about_us():
     return render_template("about_us.html")
